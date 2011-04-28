402095c14c5fb05b58f19f23ca87ea8f04f7a2fb
Will Dietz <w@wdtz.org>
2011-04-28 00:04:09 -0500

    start.sh: Integrate the x server launching code

--- armv7/start.sh
+++ armv7/start.sh
@@ -1,20 +1,25 @@
-#!/bin/sh -l
-#Launches the x server, with a particular app in it.
+#!/bin/sh
+#Launches the x server
+
+#This script should be called from anything that wants/needs an X server
+#Assumes 'DISPLAY' variable is /set/ to what we should launch the server on
+#(invoke like: 'DISPLAY=:0.0 /path/to/this/file/start.sh')
 
 #This is where the X server lives
-X_DIR=/media/cryptofs/apps/usr/palm/applications/org.webosinternals.xorg-server
-#Where the app we're invoking lives
-APP_DIR=/media/cryptofs/apps/usr/palm/applications/org.webosinternals.xterm-bin
+APP_DIR=/media/cryptofs/apps/usr/palm/applications/org.webosinternals.xserver
+KEYMAP_DIR=$APP_DIR/share/X11/xkb/keymap
 
-#XXX: We might want/need a way to get/set unique DISPLAY's for the case of launching
-#multiple servers, perhaps each with their own app.  For now it's just one-at-a-time,
-#with the nice 'fallback' of if you try to launch a new one the server will fail
-#and the client will run in the existing one.
-#(but without wm that might not be ideal, but better than crashing)
-export DISPLAY=:0.0
+export PATH=$APP_DIR/bin:$PATH
+#Portrait orientation
+Xsdl -noreset -nolisten tcp -retro -screen 320x480x24 $DISPLAY &
+#Landscape
+#Xsdl -noreset -nolisten tcp -retro -screen 480x320x24 $DISPLAY &
 
-#Start the x-server
-$X_DIR/start.sh
+sleep 1
+xkbcomp -R$KEYMAP_DIR palm-pre $DISPLAY
+
+#XXX: Add code that runs ~/.xinitrc or /media/internal/xinitrc if it exists?
+
 
 #Start xterm
 
