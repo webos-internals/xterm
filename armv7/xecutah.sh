9e3bdbffd0712589747a302f522adfd9b8122cbe
Will Dietz <w@wdtz.org>
2011-07-11 22:15:26 -0500

    Run xterm maximized! Version bump to 0.4.1

--- armv7/xecutah.sh
+++ armv7/xecutah.sh
@@ -6,12 +6,5 @@ export DISPLAY=:0.0
 
 export PATH=${APP_DIR}/bin:${PATH}
 
-# TODO: xterm shouldn't be setting any keymaps, that's the job of the server (or what runs the server)
-
-## ${APP_DIR}/bin/xkbcomp -R${APP_DIR}/share/X11/xkb/keymap palm-prex ${DISPLAY}
-## 
-## ${APP_DIR}/bin/xkbset exp 60 =sticky =twokey =latchlock
-## ${APP_DIR}/bin/xkbset sticky -twokey latchlock
-
-#This sizes the xterm, using default font, to more or less fill the screen.
-${APP_DIR}/bin/xterm -geometry 52x36+0+0 -e "login -f root" 2>&1 >& /tmp/xterm.log &
+# Run xterm maximized...
+${APP_DIR}/bin/xterm -maximize -e "login -f root" 2>&1 >& /tmp/xterm.log &
