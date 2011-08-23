f10accb155fdeddac024948843db15e746abbe61
Ryan Rix <ry@n.rix.si>
2011-08-22 20:47:21 -0700

    Enable tweaks support in the script.
    Holy crap this is ugly, can some Shell guru make this not un-repeatable?

--- xecutah.sh
+++ xecutah.sh
@@ -6,5 +6,19 @@ export DISPLAY=:0.0
 
 export PATH=${APP_DIR}/bin:${PATH}
 
+RESPONSE=$(luna-send -n 1 palm://org.webosinternals.tweaks.prefs/get '{"owner":"org.webosinternals.xterm", "keys":["fgColor","bgColor"]}')
+
+export FG="-fg $(echo $RESPONSE| cut -d, -f2|tail -c +11|sed "s/[\",{,}]//g")"
+export BG="-bg $(echo $RESPONSE| cut -d, -f3|tail -c +11|sed "s/[\",{,}]//g")"
+
 # Run xterm maximized...
-${APP_DIR}/bin/xterm -maximize -e "login -f root" &> /tmp/xterm.log &
+if [ "$FG" == "-fg " ]; then
+    export FG="";
+fi
+
+if [ "$BG" == "-bg " ]; then
+    export BG="";
+fi
+
+${APP_DIR}/bin/xterm -maximize $FG $BG -e "login -f root" &> /tmp/xterm.log &
+
