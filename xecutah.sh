1897caa760fee80104484574b1cce50893ffcdd8
Will Dietz <w@wdtz.org>
2011-08-23 21:55:59 -0500

    Refactor the color-fetching logic to a separate file. Bump to 0.7.5

--- xecutah.sh
+++ xecutah.sh
@@ -6,19 +6,7 @@ export DISPLAY=:0.0
 
 export PATH=${APP_DIR}/bin:${PATH}
 
-RESPONSE=$(luna-send -f -n 1 palm://org.webosinternals.tweaks.prefs/get '{"owner":"org.webosinternals.xterm", "keys":["fgColor","bgColor"]}')
-
-export FG="-fg $(echo "$RESPONSE"| grep fgColor|cut -d\" -f4|tr -cd 'a-zA-Z0-9')"
-export BG="-bg $(echo "$RESPONSE"| grep bgColor|cut -d\" -f4|tr -cd 'a-zA-Z0-9')"
-
-# Run xterm maximized...
-if [ "$FG" == "-fg " ]; then
-    export FG="";
-fi
-
-if [ "$BG" == "-bg " ]; then
-    export BG="";
-fi
+. ${APP_DIR}/get_colors.inc
 
 ${APP_DIR}/bin/xterm -maximize $FG $BG -e "login -f root" &> /tmp/xterm.log &
 
